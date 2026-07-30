375f377d656bacb32657a684fee34789936d06fc
Aeli <aeli@overflow.dev>
2026-07-30 12:09:43 +0000

    fix: add missing import for OverlayService

--- app/src/main/java/com/aeli/overflow/MainActivity.kt
+++ app/src/main/java/com/aeli/overflow/MainActivity.kt
@@ -3,9 +3,9 @@ package com.aeli.overflow
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import com.aeli.overflow.service.OverlayService
 
 class MainActivity : Activity() {
 
